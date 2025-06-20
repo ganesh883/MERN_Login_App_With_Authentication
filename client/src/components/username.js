c702fc8a2a71969bdb869e9c494991f83c4afb76
Ganesh K <ganesh150eee@gmail.com>
2025-06-21 00:12:15 +0530

    Username Component Updated

--- client/src/components/username.js
+++ client/src/components/username.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import '../Styles/username.css';
+import {Link} from 'react-router-dom';
+import avatar from '../assets/profile.png';
+import {Toaster} from 'react-hot-toast';
+import {useFormik} from 'formik';
+
 
 export default function username() {
+
+   const formik = useFormik({
+      initialValues :{
+        username : ''
+      },
+      validateOnBlur: false,
+      validateOnBlur : false,
+      onSubmit : async values =>{
+        console.log(values);
+      }
+   })
+
+
+
+
+
+
+
   return (
-    <div>
-      Username
+    <div className="main-container">
+      <div className="card">
+
+        <div className="title">
+          <h4>Hello Again!</h4>
+          <span>Explore More by connecting with us!</span>
+        </div>
+
+        <form className="Mainform1" onSubmit={formik.handleSubmit}>
+            <div className="Mainform2">
+                <img src={avatar} alt="avatar"></img>
+            </div>
+
+            <div className="Mainform3">
+                <input {...formik.getFieldProps('username')} type="text" placeholder="Username"></input>
+                <button type="submit">Let's Go</button>
+            </div>
+
+            <div>
+              <span>Not a Member? <Link className='Register' to="/register">Register Now!</Link></span>
+            </div>
+
+        </form>
+
+      </div>
     </div>
-  )
+  );
 }
