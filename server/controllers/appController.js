526a2aeb3a33071f7fcbeb05c459ab5ef299d21d
Ganesh K <ganesh150eee@gmail.com>
2025-06-23 17:54:23 +0530

    Created appController and updated route file

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,3 +1,42 @@
+
+/**POST: http://localhost:5000/api/register */
 export async function register(req,res){
-    res.json('register route')
+    res.json('register route');
 }
+
+/**POST: http://localhost:5000/api/login */
+export async function login(req,res){
+    res.json('login route');
+}
+
+/**PUT: http://localhost:5000/api/updateUser */
+export async function updateUser(req,res){
+    res.json('UpdateUser Route');
+}
+
+/**GET: http://localhost:5000/api/user/example123 */
+export async function getUser(req,res){
+    res.json('getUser Route');
+}
+
+/**GET: http://localhost:5000/api/generateOTP */
+export async function generateOTP(req,res){
+    res.json('generateOTP Route');
+}
+
+/**GET: http://localhost:5000/api/verifyOTP */
+export async function verifyOTP(req,res){
+    res.json('verifyOTP Route');
+}
+
+/**GET: http://localhost:5000/api/createResetSession */
+export async function createResetSession(req,res){
+    res.json('createResetSession Route');
+}
+
+/**PUT: http://localhost:5000/api/ResetPassword */
+export async function resetPassword(req,res){
+    res.json('resetPassword Route');
+}
+
+
--- server/router/route.js
+++ server/router/route.js
@@ -1,24 +1,27 @@
 import {Router} from "express";
 const router = Router();
 
+/**import all controllers  */
+import * as controller from '../controllers/appController.js'
+
 
 /**POST Methods */
-router.route('/register').post((req,res) => res.json('register route')); //register user
-router.route('/registerMail').post();//send the mail
-router.route('/authenticate').post();//authenticate user
-router.route('/login').post();//login in app
+router.route('/register').post(controller.register); //register user
+// router.route('/registerMail').post();//send the mail
+router.route('/authenticate').post((req,res)=>res.end);//authenticate user
+router.route('/login').post(controller.login);//login in app
 
 
 /**GET Methods */
-router.route('/user/:username').get();// user with username
-router.route('/generateOTP').get();//generate random OTP
-router.route('/verifyOTP').get();// verify generated OTP
-router.route('/createResetSession').get();//reset all the variables
+router.route('/user/:username').get(controller.getUser);// user with username
+router.route('/generateOTP').get(controller.generateOTP);//generate random OTP
+router.route('/verifyOTP').get(controller.verifyOTP);// verify generated OTP
+router.route('/createResetSession').get(controller.createResetSession);//reset all the variables
 
 
 /**PUT Methods */
-router.route('/updateuser').put();// Used to update the user profile
-router.route('/resetPassword').put();// used to reset password
+router.route('/updateuser').put(controller.updateUser);// Used to update the user profile
+router.route('/resetPassword').put(controller.resetPassword);// used to reset password
 
 
 
