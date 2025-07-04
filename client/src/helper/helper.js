11fec906ea292105da49e6f5258994e2f615bebe
Ganesh K <ganesh150eee@gmail.com>
2025-07-04 14:34:37 +0530

    Updated API Request with Axios

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN ;
+
 /**authenticate Function */
 export async function authenticate(username){
     try {
@@ -43,7 +45,7 @@ export async function registerUser(Credentials){
 /** Login Function */
 export async function verifyPassword({username, password}){
     try {
-        
+
         if(username){
             const { data }= await axios.post('/api/login',{username, password})
             return Promise.resolve({data});
@@ -51,5 +53,63 @@ export async function verifyPassword({username, password}){
 
     } catch (error) {
         return Promise.reject({error: "Password doesn't match..!"});
+    }
+}
+
+/**Update User Profile Function */
+export async function updateUser(response){
+    try {
+        
+        const token = await localStorage.getItem('token');
+        const data  = await axios.put('/api/updateuser', response, {headers : { "Authorization" : `Bearer ${token}`}});
+
+        return Promise.resolve({data});
+    } catch (error) {
+        return Promise.reject({ error: "Couldn't Update Profile..!"});
+    }
+}
+
+/**Generate OTP */
+export async function generateOTP(username){
+
+    try {
+        
+        const {data: {code}, status } = await axios.get('/api/generateOTP', {params : {username}});
+
+        //Send mail with OTP
+        if(status === 201){
+            let {data: {email}} = await getUser({ username });
+            let text = `Your password recovery OTP is ${code}. Verify and recover your password.`;
+            await axios.post('/api/registerMail',{username,userEmail:email, text, subject: "Password Recovery OTP"});
+        }
+
+        return Promise.resolve(code);
+
+    } catch (error) {
+        return Promise.reject({error});
+    }
+}
+
+/** Verify OTP */
+export async function verifyOTP(username, code){
+    try {
+
+        const {data,status} = await axios.get('/api/verifyOTP', {params : {username, code}});
+        return {data, status};
+        
+    } catch (error) {
+        return Promise.reject(error);
+    }
+}
+
+/** Reset  Password */
+export async function resetPassword({username, password}){
+    try {
+
+        const {data, status} =  await axios.put('/api/resetPassword', {username, password});
+        return Promise.resolve({data, status});
+
+    } catch (error) {
+        return Promise.reject({error});
     }
 }
