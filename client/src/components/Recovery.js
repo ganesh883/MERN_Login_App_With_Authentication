bfb63fbab6ceace269d7a6a9cf805d436359c389
Ganesh K <ganesh150eee@gmail.com>
2025-07-07 17:59:42 +0530

    Updated the OTP Generation and Verification

--- client/src/components/Recovery.js
+++ client/src/components/Recovery.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
 import '../Styles/Component.css';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import avatar from '../assets/profile.png';
-import {Toaster} from 'react-hot-toast';
+import toast, {Toaster} from 'react-hot-toast';
+import { useAuthStore } from "../store/store";
+import { generateOTP, verifyOTP } from "../helper/helper";
+import { useNavigate } from "react-router-dom";
 
 export default function Recovery() {
+
+  const {username} = useAuthStore(state => state.auth);
+  const [OTP, setOTP] = useState();
+  const Navigate = useNavigate();
+
+  useEffect(() => {
+    generateOTP(username).then((OTP) =>{
+      console.log(OTP);
+      if(OTP) return toast.success('OTP has been send to your email')
+        return toast.error('Problem while generating OTP..!');
+    })
+  },[username]);
+
+  async function onSubmit(e){
+
+    e.preventDefault();
+
+    try {
+      let {status} = await  verifyOTP({ username, code : OTP});
+      if(status===201){
+        toast.success('Verified Successfully!!');
+        return Navigate('/reset');
+      }
+    } catch (error) {
+      return toast.error('Wrong OTP! Check E-mail again!')
+    }    
+  }
+
+
+  //handler of resend OTP
+  function resendOTP(){
+    let sendPromise = generateOTP(username);  
+
+    toast.promise(sendPromise,{
+      loading : 'Sending..',
+      success : <b> OTP has been send to your E-mail</b>,
+      error   : <b> Could not send it</b>
+
+    });
+
+    sendPromise.then(OTP =>{
+      console.log(OTP);
+    })
+  }
 
   return (
     <div className="main-container">
@@ -16,15 +63,15 @@ export default function Recovery() {
           <span>Enter OTP to recover password</span>
         </div>
 
-        <form className="Mainform1">
+        <form className="Mainform1" onSubmit={onSubmit}>
             <div className="Mainform3">
               <span> Enter 6 digit OTP sent to your email address</span>
-                <input type="text" placeholder="Enter OTP"></input>
+                <input onChange={(e) => setOTP(e.target.value) }type="text" placeholder="Enter OTP"></input>
                 <button type="submit">Recover</button>
             </div>
 
             <div>
-              <span>Can't get OTP? <button className="resendbtn">Resend</button></span>
+              <span>Can't get OTP? <button onClick={resendOTP} className="resendbtn">Resend</button></span>
             </div>
 
         </form>
